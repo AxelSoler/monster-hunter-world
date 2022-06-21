0f0379b34fd6714705f294899d973ad2565c5c51
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-21 13:43:45 -0300

    return button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <div>
     <header>
+      <NavLink to="/"><IoIosArrowBack size="2em" color="#0290ff" /></NavLink>
       <h1>Monsters</h1>
     </header>
   </div>
